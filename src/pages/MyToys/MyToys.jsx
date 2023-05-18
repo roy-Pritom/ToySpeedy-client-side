cf66f5eab1bad6a99a4ade84602a341a1e588d1c
roy-Pritom <pritombd.hitcse2020@gmail.com>
2023-05-18 14:58:30 +0530

    add delte method in my toys

--- src/pages/MyToys/MyToys.jsx
+++ src/pages/MyToys/MyToys.jsx
@@ -4,51 +4,72 @@ import MyToysRow from "./MyToysRow";
 
 
 const MyToys = () => {
-    const {user}=useContext(authContext)
+    const { user } = useContext(authContext)
     // console.log(user);
-const [toys,setToys]=useState();
-useEffect(()=>{
-    fetch(`http://localhost:5000/myToys/${user?.email}`)
-    .then(res=>res.json())
-    .then(data=>{
-        console.log(data);
-        setToys(data)
-    })
-},[user])
+    const [toys, setToys] = useState();
+    useEffect(() => {
+        fetch(`http://localhost:5000/myToys/${user?.email}`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setToys(data)
+            })
+    }, [user])
+
+    const handleDelete=(id)=>{
+        fetch(`http://localhost:5000/myToys/${id}`,{
+         method:"DELETE"
+        })
+        .then(res=>res.json())
+        .then(item=>{
+            console.log(item)
+        })
+ 
+     }
 
     return (
         <div className="mx-12 mt-14">
-        <div className="overflow-x-auto w-full">
-  <table className="table w-full">
-    {/* head */}
-    <thead>
-      <tr>
-        <th>
-          <label>
-            <input type="checkbox" className="checkbox" />
-          </label>
-        </th>
-        <th>Image</th>
-        <th>Toy Name</th>
-        <th>Price</th>
-        <th>Update</th>
-      </tr>
-    </thead>
-    <tbody>
-    
-    {
-        toys?.map(toy=><MyToysRow
-        key={toy._id}
-        toy={toy}
-        ></MyToysRow>)
-    }
-    
-      
-    </tbody>
-   
-    
-  </table>
-</div>
+            <div className="overflow-x-auto w-full">
+                <table className="table w-full">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>
+                                <label>
+                                    <input type="checkbox" className="checkbox" />
+                                </label>
+                            </th>
+                            <th>Image</th>
+                            <th>Toy Name</th>
+                            <th>Price</th>
+                            <th>Update</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        {
+                            toys ?
+                                <>
+                                    {
+                                        toys?.map(toy => <MyToysRow
+                                            key={toy._id}
+                                            toy={toy}
+                                            handleDelete={handleDelete}
+                                        ></MyToysRow>)
+                                    }
+                                </>
+                                :
+                               
+                          <button className="btn loading">loading</button>
+
+                        }
+
+
+                    </tbody>
+
+
+                </table>
+            </div>
         </div>
     );
 };
--- src/pages/MyToys/MyToysRow.jsx
+++ src/pages/MyToys/MyToysRow.jsx
@@ -1,12 +1,13 @@
 
 
-const MyToysRow = ({ toy }) => {
-    const { image, toyName, price } = toy || {}
+const MyToysRow = ({ toy,handleDelete }) => {
+    const { image, toyName, price,_id} = toy || {}
+   
     return (
         <tr>
             <th>
                 <label>
-                    <button className="btn btn-circle btn-sm ">
+                    <button className="btn btn-circle btn-sm " onClick={()=>handleDelete(_id)}>
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
                     </button>
                 </label>
